5741187a1072d54841222828e3ee7fe70758097a
Vyctrus <and.gierlak@gmail.com>
2023-04-14 15:40:32 +0200

    First version, doesnt work as intended yet*

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Solution {
+
+
+    public static String toBinary(int num){
+        String binary="";
+        while(num>0){
+            binary= (num%2)+binary;
+            num /=2;
+        }
+        return binary;
+    }
+
     public int solution(int N){
-        return -1;
+        int maxSpace=0;
+        int currentSpace=0;
+        boolean readyToCount=false;
+        char[] number=toBinary(N).toCharArray();
+        for(int i=0;i< number.length;i++){
+            if(number[i]=='1'){
+                readyToCount=true;
+                maxSpace= (currentSpace>maxSpace)? currentSpace : maxSpace;
+            }else{
+                if(readyToCount){
+                    //we have 0 here
+                    currentSpace++;
+                }
+            }
+        }
+        return maxSpace;
     }
 }
