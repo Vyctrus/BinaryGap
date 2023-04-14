24dfae26b942174629efb8f95d3f5cfe37219517
Vyctrus <and.gierlak@gmail.com>
2023-04-14 15:48:56 +0200

    Made sure toBinary works properly

---
+++ src/test/java/testPackage/TestCase4.java
@@ -0,0 +1,39 @@
+package testPackage;
+
+import org.example.Solution;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class TestCase4 {
+    private int N;
+    private String expected;
+
+    public TestCase4(int N,String expected){
+        this.N=N;
+        this.expected=expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection parameters(){
+        return Arrays.asList(new Object[][]{
+                {529,"1000010001"},
+                {20,"10100"},
+                {15,"1111"},
+                {32,"100000"},
+                {1041,"10000010001"}
+        });
+    }
+
+    @Test
+    public void solutionTest(){
+        Solution s=new Solution();
+        System.out.println(s.toBinary(N) +"---"+expected);
+        Assert.assertTrue(s.toBinary(N).equals(expected));
+    }
+}
